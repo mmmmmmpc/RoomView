f4e6716a31786fafb19e893d2cc8c44e3c2269f6
Olga Vara Pérez <olgavara14@gmail.com>
2020-05-01 14:55:25 +0200

    Primer commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RoomView
+Proyecto final del ciclo de Desarrollo de Aplicaciones Web , donde desarrollaremos una aplicación de reserva de salas de reunión en un entono empresarial.
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+session_start();
+include_once 'App/config.php';
+include_once 'App/modelo/modeloDB.php';
+include_once 'App/controlador/controladorEmpleados.php';
+
+modeloDB::init();
+
+$path=[
+    "Inicio"     => "ctlInicio",
+    "Cerrar"     => "ctlCerrar",
+    "VerReserva" => "ctlVerReserva",
+    "Agregar"    => "ctlAgregar",
+    "Modificar"  => "ctlModificar",
+    "Borrar"     => "ctlBorrar"
+];
+
+if (!isset($_SESSION['user'])){
+    $procRuta = "ctlInicio";
+}else {
+    if(isset($_GET['orden'])){
+        // La orden tiene una funcion asociada
+        if ( isset ($path[$_GET['orden']]) ){
+            $procRuta = $path[$_GET['orden']];
+        }else {
+            header('Status: 404 Not Found');
+            echo '<html><body><h1>Error 404: No existe la ruta <i>' . $_GET['ctl'] . '</p></body></html>';
+            exit;
+           
+            // Error no existe funci�n para la ruta
+        }
+        
+   }else{
+        
+        $procRuta = "ctlVerReserva"; 
+    }
+}
+
+if (isset($_GET['msg'])){
+    $msg = $_GET['msg'];
+}
+$procRuta();
+
+?>
